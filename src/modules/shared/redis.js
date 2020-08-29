85e52cfc4b706857fa37805e12846c152cf83d2a
Lakshya Khera <lakshya.khera@gmail.com>
2020-08-30 04:38:47 +0530

    fix: Changed redis caching to 1 min

--- src/modules/shared/redis.js
+++ src/modules/shared/redis.js
@@ -2,7 +2,7 @@ const ExpressRedisCache = require('express-redis-cache')
 
 const cache = ExpressRedisCache({
   port: process.env.REDIS_PORT,
-  expire: 10, // optional: expire every 10 seconds
+  expire: 60,
 })
 
 module.exports = cache
